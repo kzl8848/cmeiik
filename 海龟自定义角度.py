543f2d8cae34952a1004ea64adcac4873f6d3470
kzl8848 <2763176797@qq.com>
2024-07-02 21:39:34 +0800

    Add files via upload

---
+++ 海龟自定义角度.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.forward(150)
+turtle.setheading(90)
+turtle.forward(150)
+turtle.setheading(180)
+turtle.forward(150)
+turtle.setheading(270)
+turtle.forward(150)
+turtle.done()
